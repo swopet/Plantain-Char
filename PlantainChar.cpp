07b93a65d53e143e5386525d3c23105696ae7e25
swopet <swopet@oregonstate.edu>
2018-06-04 19:48:45 -0700

    add cube sphere

--- PlantainChar.cpp
+++ PlantainChar.cpp
@@ -12,31 +12,89 @@
 #include <Winsock2.h>
 #include <string.h>
 
-#define SCREENCAP true
+#define SCREENCAP false
 #define PORT 9400
 #define FRAMES 3592
 sf::RenderWindow * window;
 float low_val, mid_val, high_val;
 double cam_rotate_speed = 0.5;
-double cam_move_speed = 0.05;
-double cam_pos = -12.0;
+double cam_move_speed = 0.01;
+double cam_pos = 0.0;
 double cam_rotation = 0.0;
 int screen_width = 1280;
 int screen_height = 720;
-int freq_vals[FRAMES][3];
+double freq_vals[FRAMES][3];
 bool program_done = false;
 int frame;
 
 GLfloat cube_top_face[] = {0.0,1.0,0.0,-0.5,0.5,-0.5,0.0,1.0,0.0,-0.5,0.5,0.5,0.0,1.0,0.0,0.5,0.5,0.5,0.0,1.0,0.0,0.5,0.5,-0.5};
 GLuint cube_indices[] = {0,1,2,3};
 
-class Cube {
+void draw_1x1_cube(bool outline, GLfloat color[]){
+    glInterleavedArrays(GL_N3F_V3F,0,cube_top_face);
+    glEnable(GL_COLOR_MATERIAL);
+    for (int j = 0; j < 1 || (outline && j < 2); j++){
+        glPushMatrix();
+        if (j==1) {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+            glColor3f(0.1,0.1,0.1);
+            glScalef(1.01,1.01,1.01);
+        }
+        else {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            glColor3f(color[0],color[1],color[2]);
+        }
+        for (int i = 0; i < 6; i++){
+            glPushMatrix();
+            if (i==1){
+                glRotatef(180.0,1.0,0.0,0.0);
+            }
+            else if (i!=0){
+                glRotatef(90.0*i,0.0,1.0,0.0);
+                glRotatef(90.0,1.0,0.0,0.0);   
+            }
+            glDrawElements(GL_QUADS,4,GL_UNSIGNED_INT,cube_indices);
+            glPopMatrix();
+        }
+        glPopMatrix();
+    }
+}
+
+double rot_val=0.0;
+void draw_cube_sphere(double radius){
+    rot_val += freq_vals[frame][2]/6.0;
+    int rad = (int)radius;
+    for (int x = -rad; x <= rad; x++){
+        for (int y = -rad; y <= rad; y++){
+            for (int z = -rad; z <= rad; z++){
+                double dist = sqrt(pow((double)x,2.0)+pow((double)y,2.0)+pow((double)z,2.0));
+                if (dist <= radius){
+                    glPushMatrix();
+                    glTranslatef(x,y,z);
+                    GLfloat scale = 0.7+(0.2)*sin((double)(dist+(double)frame/14.0)*M_PI/(radius));
+                    glScalef(scale,scale,scale);
+                    if (frame < 380)
+                        glRotatef((double)frame*380.0/360.0*(x%2==0 ? 1.0 : -1.0),1.0,0.0,0.0);
+                    else 
+                        glRotatef(((double)(frame)-380.0)*(y%2==0 ? 1.0 : -1.0),0.0,1.0,0.0);
+                    glRotatef(rot_val,x,y,z);
+                    GLfloat color[] = {0.6+0.4*sin((double)(dist+(double)(frame+z*7.0)/14.0)*M_PI/(radius)),0.7,0.6+0.4*cos((double)(dist+(double)(frame+x*7.0)/14.0)*M_PI/(radius))};
+                    draw_1x1_cube(true,color);
+                    glPopMatrix();
+                }
+            }
+        }
+    }
+}
+
+/*class Cube {
 private:
-    sf::Vector3f position;
+    sf::Vector3f;
     double rotate_val = 0.0;
     double rotate_speed = 0.0;
     double scale_val = 0.0;
     double distance = 0.0;
+    sf::Vector3f rot_axis;
 public:
     void set_position(sf::Vector3f new_position){
         position = new_position;
@@ -46,7 +104,7 @@ public:
         
         glLineWidth(2.0);
         glTranslatef(position.x,position.y,position.z);
-        double my_scale_val = (1.0 + scale_val*0.01);
+        double my_scale_val = (1.0 + sqrt(scale_val)*0.05);
         if (distance <= 0.5){
             glPopMatrix();
             return;
@@ -56,7 +114,7 @@ public:
             std::cerr << distance << std::endl;
         }
         glScalef(my_scale_val,my_scale_val,my_scale_val);
-        glRotatef(rotate_val,1.0,1.0,1.0);
+        glRotatef(rotate_val,rot_axis.x,rot_axis.y,rot_axis.z);
         glInterleavedArrays(GL_N3F_V3F,0,cube_top_face);
         for (int j = 0; j < 2; j++){
             glPushMatrix();
@@ -86,15 +144,79 @@ public:
         glPopMatrix();
     }
     void update(){
-        sf::Vector3f distance_vec = position - sf::Vector3f(0.0,cam_pos,0.0);
-        distance = sqrt(distance_vec.x*distance_vec.x+distance_vec.y*distance_vec.y+distance_vec.z*distance_vec.z);
+        rot_axis = position - sf::Vector3f(0.0,cam_pos,0.0);
+        distance = sqrt(rot_axis.x*rot_axis.x+rot_axis.y*rot_axis.y+rot_axis.z*rot_axis.z);
         scale_val = scale_val * 0.7 + (float)freq_vals[frame][2]*0.3;
-        rotate_speed = rotate_speed * 0.2 + (float)freq_vals[frame][0]*0.8;
+        rotate_speed = rotate_speed * 0.2 + (float)freq_vals[frame][0]*0.5*0.8;
         rotate_val += 3.0 - rotate_speed * 0.25;
     }
 };
 
-Cube cubes[512];
+class Cube_Tube{
+private:
+    Cube cubes[8][26];
+public:
+    void init(){
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j < 26; j++){
+                if (j < 5){
+                    cubes[i][j].set_position(sf::Vector3f(5.0,0.0,0.0));
+                }
+                else if (j < 13){
+                    cubes[i][j].set_position(sf::Vector3f(8.0,0.0,0.0));
+                }
+                else if (j < 26){
+                    cubes[i][j].set_position(sf::Vector3f(13.0,0.0,0.0));
+                }
+            }
+        }
+    }
+    void draw(){
+        for (int i = 0; i < 8; i++){
+            glPushMatrix();
+            glTranslatef(0.0,i*2.0,0.0);
+            for (int j = 0; j < 26; j++){
+                glPushMatrix();
+                if (j < 5){
+                    glRotatef(j*72.0,0.0,1.0,0.0);
+                    if (i%2==0){
+                        glScalef(-1.0,1.0,1.0);
+                        glRotatef(-cam_rotation,0.0,1.0,0.0);
+                    }
+                }
+                else if (j < 13){
+                    glRotatef((j-5)*360.0/8.0,0.0,1.0,0.0);
+                    if (i%2!=0){
+                        glScalef(-1.0,1.0,1.0);
+                        glRotatef(-cam_rotation,0.0,1.0,0.0);
+                    }
+                }
+                else {
+                    glRotatef((j-13)*360.0/13.0,0.0,1.0,0.0);
+                    if (i%2==0){
+                        glScalef(-1.0,1.0,1.0);
+                        glRotatef(-cam_rotation,0.0,1.0,0.0);
+                    }
+                }
+                cubes[i][j].draw();
+                glPopMatrix();
+            }
+            glPopMatrix();
+        }
+    }
+    void update(){
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j < 26; j++){
+                cubes[i][j].update();
+            }
+        }
+        
+    }
+};
+
+Cube_Tube cube_tube;*/
+
+GLfloat light_magnitude = 0.0;
 
 void display(){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -112,26 +234,30 @@ void display(){
     //MODIFY THE CAMERA HERE
     glRotatef(cam_rotation,0.0,1.0,0.0);
     glTranslatef(0.0,-cam_pos,0.0);
-    
     //EDIT LIGHTING HERE
+    light_magnitude = 1.0;
+    GLfloat light_ambient[4];
+    GLfloat light_diffuse[] = {light_magnitude,light_magnitude,light_magnitude,1.0};
+    GLfloat light_specular[] = {light_magnitude,light_magnitude,light_magnitude,1.0};
     GLfloat light_position[] = {0.0, cam_pos, 0.0, 1.0};
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
     glLightfv(GL_LIGHT0, GL_POSITION, light_position);
     glEnable(GL_LIGHT0);
     
     //DISPLAY STUFF HERE
-    
-	for (int i = 0; i < 512; i++){
-        cubes[i].draw();
-    }
+    glTranslatef(0.0,0.0,-24.0);
+    glRotatef(frame,1.0,1.0,1.0);
+    GLfloat color[] = {1.0,1.0,1.0};
+	//draw_1x1_cube(true,color
+    draw_cube_sphere(7.5);
 }
 
 void update(){
     frame++;
-    for (int i = 0; i < 512; i++){
-        cubes[i].update();
-    }
-    cam_pos += cam_move_speed;
-    cam_rotation += cam_rotate_speed;
+    //cam_pos += cam_move_speed;
+    //cam_rotation += cam_rotate_speed;
 }
 
 void init(){
@@ -144,7 +270,7 @@ void init(){
         for (int j = 0; j < 3; j++){
             while (true){
                 if (ind == in.length() || in[ind] == ','){
-                    freq_vals[i][j] = curr_val;
+                    freq_vals[i][j] = (double)curr_val;
                     curr_val = 0;
                     ind++;
                     break;
@@ -154,15 +280,8 @@ void init(){
                     curr_val += (in[ind]-'0');
                     ind++;
                 }
-                
             }
-            std::cout << freq_vals[i][j] << "\t";
         }
-        std::cout << std::endl;
-    }
-    
-    for (int i = 0; i < 512; i++){
-        cubes[i].set_position(sf::Vector3f(((float)(i%8)-3.5)*2.0,((float)((i/8)%8)-3.5)*2.0,((float)(i/64)-3.5)*2.0));
     }
     glClearColor(0.0,0.0,0.0,1.0);
 	glViewport(0.0,0.0,1280,720);
@@ -218,6 +337,7 @@ int main(int argc, char **argv) {
         update();
     	display();
 #if SCREENCAP == true
+        if (screenshot_count%10==0) printf("frame %d\n",screenshot_count);
         sf::Vector2u windowSize = window->getSize();
         sf::Texture new_tex;
         new_tex.create(windowSize.x,windowSize.y);
@@ -227,6 +347,7 @@ int main(int argc, char **argv) {
         file_name << "screenshots/frame_" << screenshot_count << ".png";
         if (screenshot.saveToFile(file_name.str())){
             screenshot_count++;
+            
         }
 #else
     	
